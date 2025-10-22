341b9201a92458894b6630c66fa434747dcd1798
Shravan Lunawat <120239674+shravan160106@users.noreply.github.com>
2025-10-22 12:38:19 +0530

    Initial commit of DSA structure

---
+++ Recursion/509 Fibonaaci Number .java
@@ -0,0 +1,11 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        int f1 = fib(n-1);
+        int f2 = fib(n-2);
+        int f = f1 + f2;
+        return f;
+    }
+}
